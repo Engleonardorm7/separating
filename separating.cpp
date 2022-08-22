6c0d746abda69106154ed489c79148bd5c3bf056
Leonardo Rodriguez <leonardorm7@hotmail.com>
2022-08-22 12:21:39 -0500

    Finished

--- separating.cpp
+++ separating.cpp
@@ -7,29 +7,31 @@ using namespace std;
 int main(int argc, char** argv) {
 	
 	
-	string characters;//[100];
+	string characters;
 	string numeros="";
 	string letras="";
 	cout<<"Type"<<endl;
 	getline(cin, characters);
-	cin.ignore();
 	int espacio=0;
 
 
-	for(int i=0; i<sizeof(characters);i++){
+	for(int i=0; i<=(sizeof(characters));i++){
 	if(characters[i]==32){
 		espacio=espacio+1;
 	}
-	
-	//	cout<<characters[i];
+			
 	if(isdigit(characters[i])){
 		numeros=numeros+characters[i];
 		
+	}else{
+		letras=letras+characters[i];
 	}
-}
-cout<<"espacios "<<espacio<<endl;
+	cout<<characters[i]<<endl;
+	}
+
+	cout<<"espacios "<<espacio<<endl;
 	cout<<"numeros: "<<numeros<<endl;
-//	cout<<"letras: "<<letras<<endl;
+	cout<<"letras: "<<letras<<endl;
 	
 	return 0;
 }
